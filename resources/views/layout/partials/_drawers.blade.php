d633daf33b786bf3d4dd087043fd7a183286eb01
Gholamreza Fadakar <fadakargholamreza@gmail.com>
2022-12-09 15:33:39 +0330

    import metronic layouts

---
+++ resources/views/layout/partials/_drawers.blade.php
@@ -0,0 +1,5 @@
+<!--begin::Drawers-->
+<!--layout-partial:partials/drawers/_activity-drawer.blade.php-->
+<!--layout-partial:partials/drawers/_chat-messenger.blade.php-->
+<!--layout-partial:partials/drawers/_shopping-cart.blade.php-->
+<!--end::Drawers-->
---
+++ resources/views/layout/partials/search/_inline.blade.php
@@ -0,0 +1,18 @@
+<!--begin::Search-->
+<div id="kt_header_search" class="header-search d-flex align-items-center w-100" data-kt-search-keypress="true" data-kt-search-min-length="2" data-kt-search-enter="enter" data-kt-search-layout="menu" data-kt-search-responsive="false" data-kt-menu-trigger="auto" data-kt-menu-permanent="true" data-kt-menu-placement="bottom-start">
+	<!--layout-partial:partials/search/partials/_form-inline.blade.php-->
+	<!--begin::Menu-->
+	<div data-kt-search-element="content" class="menu menu-sub menu-sub-dropdown w-300px w-md-350px py-7 px-7 overflow-hidden">
+		<!--begin::Wrapper-->
+		<div data-kt-search-element="wrapper">
+			<!--layout-partial:partials/search/partials/_results.blade.php-->
+			<!--layout-partial:partials/search/partials/_main.blade.php-->
+			<!--layout-partial:partials/search/partials/_empty.blade.php-->
+		</div>
+		<!--end::Wrapper-->
+		<!--layout-partial:partials/search/partials/_advanced-options.blade.php-->
+		<!--layout-partial:partials/search/partials/_preferences.blade.php-->
+	</div>
+	<!--end::Menu-->
+</div>
+<!--end::Search-->
