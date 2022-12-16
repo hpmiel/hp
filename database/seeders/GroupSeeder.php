674bf01e443358f4ed2fecfb31a4e1301bad6e53
Gholamreza Fadakar <fadakargholamreza@gmail.com>
2022-12-16 17:08:54 +0330

    fix Column length db

---
+++ database/seeders/GroupSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Group;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class GroupSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $groupAdmin = new Group();
+        $groupAdmin->key = 'admin';
+        $groupAdmin->name = 'گروه مدیر';
+        $groupAdmin->description = 'گروه مدیریت کل پنل';
+        $groupAdmin->save();
+
+        $groupAnbar = new Group();
+        $groupAnbar->key = 'anbar';
+        $groupAnbar->name = 'گروه انبار';
+        $groupAnbar->description = 'گروه انبار ';
+        $groupAnbar->save();
+
+    }
+}
