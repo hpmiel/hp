b7afc2ab3ea1795b54479a02d9c61bf0c7347b07
hoda <md.hp70@gmail.com>
2022-12-21 11:12:46 -0800

    develop user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,21 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('group_id',false,true);
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('email',225)->unique();
+            $table->timestamp('email_verified_at')->unique()->nullable();
+            $table->string('password')->nullable();
+            $table->string('cellphone')->nullable();
             $table->timestamps();
+
+
+            $table->foreign('group_id')
+                ->on('group')
+                ->references('id')
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+
         });
     }
 
