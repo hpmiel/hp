c6e5bd570f80a032cd2226bed2ea11d7d1da469e
hoda <md.hp70@gmail.com>
2022-12-29 06:43:50 -0800

    create ressource route for usercontroller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
        return view('dashboard.index');
 });
+
+Route::group(['prefix'=>'user'],function(){
+    Route::get('/',[UserController::class,'index']);
+    Route::get('create',[UserController::class,'create']);
+    Route::post('create',[UserController::class,'store']);
+    Route::get('{id}/edit',[UserController::class,'store']);
+    Route::post('{id}/edit',[UserController::class,'update']);
+    Route::get('{id}/delete',[UserController::class,'destroy']);
+});
+
+
