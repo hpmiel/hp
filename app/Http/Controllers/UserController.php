a3610f35bf35600298e44cdd7861eea0fead65ee
hoda <md.hp70@gmail.com>
2022-12-29 06:42:56 -0800

    create user controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    // /user
+    public function index()
+    {
+
+    }
+
+    // /user/create
+    public function create()
+    {
+        //
+    }
+
+    // /user/create [ POST]
+    public function store(Request $request)
+    {
+
+    }
+
+    // /user/200/edit
+    public function edit($id)
+    {
+
+    }
+    // /user/200/edit [ POST]
+    public function update(Request $request, $id)
+    {
+
+    }
+
+    // /user/200/delete
+    public function destroy($id)
+    {
+
+    }
+}
